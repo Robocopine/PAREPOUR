dba6c9b6e3ec1dbaf3e6fb22259382104322db45
Robocopine <julie@bottcher.ch>
2024-04-04 11:33:28 +0200

    edit forn member$

--- assets/admin/admin.js
+++ assets/admin/admin.js
@@ -15,6 +15,3 @@ import '../bootstrap';
 
 require('@fortawesome/fontawesome-free/css/all.min.css');
 require('@fortawesome/fontawesome-free/js/all.js');
-const $ = require('jquery');
-
-global.$ = global.jQuery = $;
